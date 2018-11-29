a75e3d1bf611f9ad9a5544a53d3f40cb4238d8d3
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-11-28 20:15:25 -0800

    Dynamic UI. Changing score and progressBar.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let allQuestions = QuestionBank()
     var pickedAnswer : Bool = false
     var questionNumber : Int = 0 // keeping track of state/question
+    var score : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -22,9 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstQuestion = allQuestions.list[0]
-        questionLabel.text = firstQuestion.questionText
-        
+        nextQuestion()
     }
 
     @IBAction func answerPressed(_ sender: AnyObject) {
@@ -41,16 +40,18 @@ class ViewController: UIViewController {
         nextQuestion()
     }
     
-    
     func updateUI() {
-      
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / 13"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
-    
 
     func nextQuestion() {
         
         if questionNumber <= 12 {
             questionLabel.text = allQuestions.list[questionNumber].questionText
+            
+            updateUI()
         } else {
             let alert = UIAlertController(title: "Awesome", message: "You've finished the quiz, do you want to start over?", preferredStyle: .alert)
             let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { (UIAlertAction) in self.startOver()
@@ -68,6 +69,9 @@ class ViewController: UIViewController {
         
         if correctAnswer == pickedAnswer {
             print("Correct!")
+            score += 1
+            print(score)
+            updateUI()
         } else {
             print("Wrong answer!")
         }
@@ -78,6 +82,7 @@ class ViewController: UIViewController {
     
     func startOver() {
         questionNumber = 0
+        score = 0
         nextQuestion()
     }
     
