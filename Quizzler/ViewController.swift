83b29b144660ed3d1dd3b914bf44a1633db5c3eb
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-11-28 19:25:42 -0800

    Added functionality to the buttons. Quiz is in an infinite loop.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0 // keeping track of state/question
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +22,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
     }
 
-
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        
+        
+        questionNumber += 1
+        nextQuestion()
     }
     
     
@@ -36,11 +49,24 @@ class ViewController: UIViewController {
 
     func nextQuestion() {
         
+        if questionNumber <= 12 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        }
+        print("End of Quiz")
+        questionNumber = 0
     }
     
     
     func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionNumber].answer
         
+        if correctAnswer == pickedAnswer {
+            print("Correct!")
+        } else {
+            print("Wrong answer!")
+        }
+        
+        nextQuestion()
     }
     
     
